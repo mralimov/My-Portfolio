e393aed94b7b479f36a6af44f222ca608575b3da
mralimov <mralimov89@gmail.com>
2021-05-06 20:00:38 -0400

    project hover title and Projects database

---
+++ JS/data.js
@@ -0,0 +1,250 @@
+gitHubRepos = {
+  donnieBank: {
+    id: 0,
+    title: 'Donnie-Bank Transactions',
+    tag: 'BANK TRANSACTIONS',
+    details:
+      'Donnie-Bank  is a app that lets customers to login tho thier bank account and see the all the transactions, request a loan or transfer money to frined who uses same bank',
+    link: 'https://donnie-bank-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 1,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 2,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 3,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 4,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 5,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 6,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 7,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 8,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 9,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 10,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 11,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 12,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 13,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 14,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 15,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 16,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 17,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 18,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 19,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 20,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 21,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 22,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 23,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 24,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 25,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 26,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 27,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 28,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 29,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+  randomDiceGame: {
+    id: 30,
+    title: 'Random Dice Game',
+    tag: 'DICE GAME FOR TWO PEOPLE',
+    details:
+      'Random Dice game lets two people to play. Player rolls the dice until gets to 20 points. If player hit number 1 looses all the accumelated points. Player has option of holding current points and pass the turn other player.',
+    link: 'https://random-dice-number-js.netlify.app/',
+  },
+};
