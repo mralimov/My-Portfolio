17b046195f8b088d72713d74f8cd1893a1f3e662
mralimov <mralimov89@gmail.com>
2021-05-11 15:35:19 -0400

    Update files renamed or deleted

--- ProjectsFilterJSX.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProjectsMenu from '../components/ProjectsMenu';
-import myRepos from '../JS/data';
-function ProjectsHead() {
-  state = {};
-  return React.createElement(ProjectsMenu, null);
-}
-
-ReactDOM.render(React.createElement(ProjectsHead, null), document.getElementById('projects'));
--- src/ProjectsFilterJSX.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProjectsMenu from '../components/ProjectsMenu';
-import myRepos from '../JS/data';
-function ProjectsHead() {
-  state = {};
-  return <ProjectsMenu />;
-}
-
-ReactDOM.render(<ProjectsHead />, document.getElementById('projects'));
