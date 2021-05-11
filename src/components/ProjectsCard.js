5e97654c278f3f06f9236c718200e962d3d51609
mralimov <mralimov89@gmail.com>
2021-05-11 15:37:56 -0400

    Repos Card component

---
+++ src/components/ProjectsCard.js
@@ -0,0 +1,120 @@
+import React from 'react';
+
+const ReposCard = () => {
+  return (
+    <div className='projects-box'>
+      <div className='filter js'>
+        <div className='card-img'>
+          <img className='project-img' src='./IMG/JS-BANK.JPG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Donnie-Bank</div>
+          <span className='highlight'>HTML CSS/SASS</span>
+        </div>
+        <button className='btn-learn-more'>LEARN MORE</button>
+      </div>
+
+      <div className='filter react'>
+        <div className='card-img'>
+          <img
+            className='project-img'
+            src='./Productivity booster.PNG'
+            alt=''
+          />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <button className='btn-learn-more'>LEARN MORE</button>
+      </div>
+
+      <div className='filter python'>
+        <div className='card-img'>
+          <img
+            className='project-img'
+            src='./Password Generator JS.PNG'
+            alt=''
+          />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+
+      <div className='filter react'>
+        <div className='card-img'>
+          <img
+            className='project-img'
+            src='./My Portfolio Bootstrapt.PNG'
+            alt=''
+          />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+
+      <div className='filter js'>
+        <div className='card-img'>
+          <img className='project-img' src='./Day planner JS.PNG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+
+      <div className='filter python'>
+        <div className='card-img'>
+          <img className='project-img' src='./Groceteria.PNG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+
+      <div className='filter python'>
+        <div className='card-img'>
+          <img className='project-img' src='./Weather App Web API.PNG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <button className='btn-learn-more'>LEARN MORE</button>
+      </div>
+
+      <div className='filter js'>
+        <div className='card-img'>
+          <img className='project-img' src='./Weather App Web API.PNG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+
+      <div className='filter react'>
+        <div className='card-img'>
+          <img className='project-img' src='./Weather App Web API.PNG' alt='' />
+        </div>
+        <div className='img-text'>
+          <div className='project-head'>Project Name</div>
+          <span className='highlight'>JS/ NODE.JS</span>
+        </div>
+        <div className='btn-learn-more'>LEARN MORE</div>
+      </div>
+    </div>
+  );
+};
+
+export default ReposCard;
