bba3b0fe888e4d4c547e839d6eafd2f5e1779de9
mralimov <mralimov89@gmail.com>
2021-05-10 22:19:09 -0400

    compile react jsx

---
+++ src/ProjectsFilterJSX.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProjectsMenu from '../components/ProjectsMenu';
+
+function ProjectsHead() {
+  return <ProjectsMenu />;
+}
+
+ReactDOM.render(<ProjectsHead />, document.getElementById('projects'));
