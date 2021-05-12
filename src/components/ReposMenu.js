2cd7ee7dadadd48401013b49343246bc433e5b11
mralimov <mralimov89@gmail.com>
2021-05-12 01:54:25 -0400

    Repos Menu list component

---
+++ src/components/ReposMenu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ReposMenu = ({ filterItem, allRepos }) => {
+  return (
+    <>
+      <div className='projects-menu'>
+        <div className='projects-menu-container'>
+          {allRepos.map((curEl, i) => {
+            return (
+              <button
+                key={i}
+                className='btn'
+                // data-filter='all'
+                onClick={() => filterItem(curEl)}
+              >
+                {curEl}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ReposMenu;
