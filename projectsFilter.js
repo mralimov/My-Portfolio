80472a8a84366b9c03dcccce5b6fbfa1992891a2
mralimov <mralimov89@gmail.com>
2021-05-12 01:58:37 -0400

    JSX compiler update

--- projectsFilter.js
+++ projectsFilter.js
@@ -4,15 +4,16 @@ var _react = _interopRequireDefault(require("react"));
 
 var _reactDom = _interopRequireDefault(require("react-dom"));
 
-var _ProjectsMenu = _interopRequireDefault(require("./components/ProjectsMenu"));
+var _ReposComponent = _interopRequireDefault(require("./components/ReposComponent"));
 
-var _ProjectsCard = _interopRequireDefault(require("./components/ProjectsCard"));
+var _ReposGallery = _interopRequireDefault(require("./components/ReposGallery"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+// import ReposCard from './components/ProjectsCard';
 // import myRepos from '../JS/data';
 function ProjectsHead() {
-  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement(_ProjectsMenu["default"], null), /*#__PURE__*/_react["default"].createElement(_ProjectsCard["default"], null));
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement(_ReposComponent["default"], null));
 }
 
 console.log('RUNNING....'); // const header = <h2>Hello World</h2>;
