5e8de96f9fd4068af85cd8c4392283de5715de75
mralimov <mralimov89@gmail.com>
2021-05-20 09:46:22 -0400

    debug webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,27 +11,12 @@ module.exports = {
     filename: '{projectsFilter.js',
   },
   module: {
-
-    loaders: [{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }],
-
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
-
-      {
-        test: /\.(jpg|png|svg|gif)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'imgs',
-          },
-        },
-      },
-
     ],
   },
   // devtool: 'cheap-module-eval-source-map',
