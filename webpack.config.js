8c09168c30f8a28c30578baa13d0be2304c1f0af
mralimov <mralimov89@gmail.com>
2021-05-28 00:02:14 -0400

    webpack update file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,22 +13,17 @@ module.exports = {
   module: {
     rules: [
       {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          publicPath: '/',
-        },
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        use: {
-          loader: 'file-loader',
-        },
       },
     ],
   },
