67afcd762f33ed1ac41180499e3649d568d50fad
mralimov <mralimov89@gmail.com>
2021-05-28 21:01:41 -0400

    refactoring repos menu

--- src/components/ReposMenu.js
+++ src/components/ReposMenu.js
@@ -1,33 +1,23 @@
 import React, { useState } from 'react';
 
 const ReposMenu = ({ filterItem, allRepos }) => {
-  // const [item, setItem] = useState('');
-
-  // function addActive() {
-  //   const toggling = document
-  //     .querySelector('.btn')
-  //     .classList.toggle('btn-active');
-  //   setItem(toggling);
-  // }
   return (
-    <>
-      <div className='projects-menu'>
-        <div className='projects-menu-container'>
-          {allRepos.map((curEl, i) => {
-            return (
-              <button
-                key={i}
-                className='btn'
-                // data-filter='all'
-                onClick={() => filterItem(curEl)}
-              >
-                {curEl}
-              </button>
-            );
-          })}
-        </div>
+    <div className='projects-menu'>
+      <div className='projects-menu-container'>
+        {allRepos.map((curEl, i) => {
+          return (
+            <button
+              key={i}
+              className='btn'
+              // data-filter='all'
+              onClick={() => filterItem(curEl)}
+            >
+              {curEl}
+            </button>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
