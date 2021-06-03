a50f7b1ad102662d2501f079bfe5c4249fbcf61c
mralimov <mralimov89@gmail.com>
2021-06-03 12:35:46 -0400

    ModalWindow component

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -1,60 +1,79 @@
-import React, { useState } from 'react';
-import './ModalWindow.css';
-import { FaChevronRight, FaChevronLeft, FaTimes } from 'react-icons/fa';
-
-const ModalWindow = (props) => {
+import React, { useState, Fragment } from 'react';
+// import { FaChevronRight, FaChevronLeft, FaTimes } from 'react-icons/fa';
+const ModalWindow = ({ dataModal, closeModal }) => {
+  console.log(dataModal);
+  const modalClosed = () => {
+    closeModal(false);
+  };
   return (
-    <div className='modal-wrap flex visible'>
-      <div className='mask'></div>
-      <div className='modal' id='modal' style={{ maxWidth: '700px' }}>
-        <div className='carousel-wrap' style={{ width: '700px' }}>
-          <div className='window'>
-            <div
-              id='carousel'
-              style={{ width: '-700px; transform: translateX(0px)' }}
-              className
-            >
-              <img
-                className='slide'
-                style={{ width: '700px' }}
-                src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'
-                alt=''
-              />
+    <div className='modal-wrap flex visible' key={Math.random().toString()}>
+      <div
+        className='mask'
+        onClick={modalClosed}
+        key={Math.random().toString()}
+      ></div>
+      <div
+        className='modal'
+        id='modal'
+        style={{ maxWidth: '700px' }}
+        key={Math.random().toString()}
+      >
+        {dataModal.map((data, i) => {
+          const { id, title, image, link, tag, details } = data;
 
-              <img
-                className='slide'
-                style={{ width: '700px' }}
-                src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-mont-blanc.jpg'
-                alt=''
-              />
+          return (
+            <Fragment>
+              <div
+                key={i}
+                className='carousel-wrap'
+                style={{ width: '700px', height: '450px' }}
+              >
+                <div className='window'>
+                  <div
+                    id='carousel'
+                    style={{ width: '-700px; transform: translateX(0px)' }}
+                    className
+                  >
+                    <img
+                      className='slide'
+                      style={{ width: '700px', height: '450px' }}
+                      src={image}
+                      alt=''
+                    />
 
-              <img
-                className='slide'
-                style={{ width: '700px' }}
-                src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-amiens-riquewihr.jpg'
-                alt=''
-              />
-            </div>
-            <i className='fas fa-chevron-left' id='prev'></i>
-            <i className='fas fa-chevron-right' id='next'></i>
-          </div>
-        </div>
-        <div className='info-box'>
-          <div className='modal_title'>MODAL TITLE</div>
-          <div className='modal_tag'>Modal Tag</div>
-          <div className='modal_detail'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-            ullam animi officiis laudantium enim quasi aut voluptatem accusamus
-            dignissimos sapiente perspiciatis ut autem, amet a.
-          </div>
-          <a href='#' target='_blank'>
-            <div className='button visible'>
-              <i className='fas fa-external-link-alt'></i>
-              VIEW SITE
-            </div>
-          </a>
-        </div>
-        <i className='close fas fa-times'></i>
+                    <img
+                      className='slide'
+                      style={{ width: '700px', height: '450px' }}
+                      src={image}
+                      alt=''
+                    />
+
+                    <img
+                      className='slide'
+                      style={{ width: '700px', height: '450px' }}
+                      src={image}
+                      alt=''
+                    />
+                  </div>
+                  <i className='fas fa-chevron-left' id='prev'></i>
+                  <i className='fas fa-chevron-right' id='next'></i>
+                </div>
+              </div>
+              <div className='info-box'>
+                <div className='modal_title'>{title}</div>
+                <div className='modal_tag'>{tag}</div>
+                <div className='modal_detail'>{details}</div>
+                <a href={link} target='_blank'>
+                  <div className='button visible'>
+                    <i className='fas fa-external-link-alt'></i>
+                    VIEW SITE
+                  </div>
+                </a>
+              </div>
+              <i className='close fas fa-times' onClick={modalClosed}></i>
+            </Fragment>
+          );
+        })}
       </div>
     </div>
   );
